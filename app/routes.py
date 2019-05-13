f2565ba913f81a877e5c75fe3313c0b240067774
Brett <brett@brettfreeman.io>
2019-05-12 21:27:35 -0400

    Basic templating.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return 'Hello world.'
+    user = {'username': 'Brett'}
+    return render_template('index.html', title='Home', user=user)
