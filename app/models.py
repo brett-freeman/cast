0a59189dae71c5ef35e87ae6189f62c6f980a327
Brett <brett@brettfreeman.io>
2019-05-23 02:41:39 +0000

    Removed migrations from version control and fixed some typos in models.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 from datetime import datetime
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     password_hash = db.Column(db.String(100))
     username = db.Column(db.String(1000), unique=True)
@@ -20,10 +21,23 @@ def load_user(id):
     return User.query.get(int(id))
 
 class Pick(db.Model):
+    __tablename__ = 'picks'
     id = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.String(140))
     album = db.Column(db.String(140))
     song = db.Column(db.String(140))
     description = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    cast_id = db.Column(db.Integer, db.ForeignKey('casts.id'))
+
+
+class Cast(db.Model):
+    __tablename__ = 'casts'
+    id = db.Column(db.Integer, primary_key=True)
+    cast_number = db.Column(db.Integer, unique=True)
+    description = db.Column(db.Text)
+
+    host_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    picks = db.relationship('Pick', backref='cast', lazy='dynamic')
