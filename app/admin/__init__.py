b6d281ebeda349194221bf2e26a069d06730b2ce
Brett <brett@brettfreeman.io>
2019-05-23 04:18:01 +0000

    Added admin_required decorator for admin pages.

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,3 +1,16 @@
-from flask import Blueprint
+from functools import wraps
+from flask import Blueprint, redirect, flash, url_for
+from flask_login import current_user
+
 admin = Blueprint('admin', __name__)
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin:
+            flash('That required admin.')
+            return redirect(url_for('main.index'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 from . import routes
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,7 +1,8 @@
-from . import admin
+from . import admin, admin_required
 from flask_login import login_required
 
 @admin.route('/admin')
 @login_required
+@admin_required
 def admin():
     return 'Hey this is some admin shit yo'
