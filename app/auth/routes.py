fb62b26eb1fecf2d35e1013071d1365bc75f7d12
Brett <brett@brettfreeman.io>
2019-05-26 20:26:08 +0000

    Taking design opinion out of the equation for now. Focusing on core functionality. Anyone wanna work on a template?

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -39,7 +39,7 @@ def register():
 
     form = RegisterForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data)
+        user = User(username=form.username.data, is_admin=False)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
